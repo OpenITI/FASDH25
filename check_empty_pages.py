9f342cd8746f2d16d2907bc72c056876ee475635
pverkind <peter.verkinderen@gmail.com>
2025-03-05 10:13:18 +0100

    Add first converted texts + scripts

---
+++ check_empty_pages.py
@@ -0,0 +1,21 @@
+import os
+import re
+
+
+start = "mARkdown"
+for folder in os.listdir(start):
+    group_folder = os.path.join(start, folder)
+    for fn in os.listdir(group_folder):
+        if fn.endswith(".yml"):
+            continue
+        fp = os.path.join(group_folder, fn)
+        if os.path.isdir(fp):
+            continue
+        with open(fp, mode="r", encoding="utf-8") as file:
+            text = file.read()
+            n_pages = len(re.findall("PageV", text))
+            n_empty = len(re.findall("!\[image filename.+[\n#~ ]+Page", text))
+        if n_empty > 2:
+            print(fp)
+            print(f"{n_empty}/{n_pages} pages empty")
+            
