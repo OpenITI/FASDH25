d85fb1c6af86b2a5007c2dee1404b19523652bc1
masoumeh Seydi <masoumeh.seydi@gmail.com>
2025-03-21 14:35:17 +0100

    teacher's sample script

---
+++ python_exercises/lesson_7.1/teachers/Masoumeh_count_titles.py
@@ -0,0 +1,20 @@
+filename = "1332ShibliNucmani.SafarnamaRumMisrSham.FASDH2025019-urd1"
+with open(filename, 'r', encoding='utf8') as file:
+    lines = file.readlines()
+
+#print(lines[:5])
+word_counts = []
+matching_lines = []
+for heading in lines:
+    if "###" in heading:
+        #print(heading)
+        matching_lines.append(heading)
+
+#print(matching_lines[:5])
+
+for heading in matching_lines:
+    print("heading: ", heading)
+    words = heading.split()
+    count_words = len(words)
+    print("Count of the words in heading: ", count_words)
+
