1887a92505ce625e66e87b1a0cbcd3c2b140504a
Mahpara123708 <mahpara.karim7@gmail.com>
2025-03-24 20:35:19 +0500

    Added homework script and output file

---
+++ python_exercises/lesson_7.1/daughters_of_spring/mahpara_karim_count_titles_output.py
@@ -0,0 +1,96 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Haroon Traders\Downloads\FASDH25\python_exercises\lesson_7.1\daughters_of_spring\mahpara_karim_homework.py
+
+First 3 headings:
+######OpenITI#
+### | حرفِ آغاز
+### |  مير امّن اور فورٹ وليم کالج
+
+Word count in each heading:
+
+Current heading: ######OpenITI#
+Count of words in this line: 1
+14
+
+Current heading: ### | حرفِ آغاز
+Count of words in this line: 4
+3
+1
+4
+4
+
+Current heading: ### |  مير امّن اور فورٹ وليم کالج
+Count of words in this line: 8
+3
+1
+3
+4
+3
+4
+4
+4
+
+Current heading: ### |  شروع کے قصّے
+Count of words in this line: 5
+3
+1
+4
+2
+4
+
+Current heading: ### | سير پہلے درويش کی
+Count of words in this line: 6
+3
+1
+3
+4
+5
+2
+
+Current heading: ### | سير دوسرے درويش کی
+Count of words in this line: 6
+3
+1
+3
+5
+5
+2
+
+Current heading: ### | حقیقت پارہ لعل کی
+Count of words in this line: 6
+3
+1
+5
+4
+3
+2
+
+Current heading: ### | سیر تیسرے درویش کی
+Count of words in this line: 6
+3
+1
+3
+5
+5
+2
+
+Current heading: ### | چھتے درویش کی سیر
+Count of words in this line: 6
+3
+1
+4
+5
+2
+3
+
+First-level headings:
+### | حرفِ آغاز
+### |  مير امّن اور فورٹ وليم کالج
+### |  شروع کے قصّے
+### | سير پہلے درويش کی
+### | سير دوسرے درويش کی
+### | حقیقت پارہ لعل کی
+### | سیر تیسرے درویش کی
+### | چھتے درویش کی سیر
