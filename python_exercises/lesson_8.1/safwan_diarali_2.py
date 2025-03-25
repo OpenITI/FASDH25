4843ba934bd6157131697dda822228bf77565ac6
sdasy-git <sdasy41973@gmail.com>
2025-03-25 16:39:19 +0500

    Added new files

--- python_exercises/lesson_8.1/safwan_diarali_2.py
+++ python_exercises/lesson_8.1/safwan_diarali_2.py
@@ -48,4 +48,15 @@ n_matches = len(matches)
 print(n_matches)
 print(f"There are {n_matches} of {pattern} in the article {filename}")
 
+splitter_pattern = r"\n+-+\n+"
+split_text = re.split(splitter_pattern, text)
+title = split_text[0]
+body = split_text[1]
 
+matches = re.findall(pattern, body)
+n_matches = len(matches)
+print(f"There are {n_matches} of {pattern} in the article body {filename}")
+      
+matches = re.findall(pattern, title)
+n_matches = len(matches)
+print(f"There are {n_matches} of {pattern} in the article title {filename}")
