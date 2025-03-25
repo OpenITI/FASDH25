e40b8d56438ea552cbb62050f7299bde4e391470
Muhammad Faheem <mohdfaheem.41826@gmail.com>
2025-03-25 16:35:26 +0500

    add my file

---
+++ python_exercises/lesson_8.1/muhammad_faheem_2.py
@@ -0,0 +1,184 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import re
+s="this, needs, to, be, splitted!"
+s="this, needs, to, be,splitted!"
+print(re.split(r",\s*",s))
+['this', 'needs', 'to', 'be', 'splitted!']
+import os
+dir = "Downloads/FASDH25/python_exercises"
+myfolder = "manto_boys"
+print(os.path.join(dir, myfolder))
+Downloads/FASDH25/python_exercises\manto_boys
+print(os.listdir(os.path.join(dir, "lesson_8.1", myfolder)))
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    print(os.listdir(os.path.join(dir, "lesson_8.1", myfolder)))
+FileNotFoundError: [WinError 3] The system cannot find the path specified: 'Downloads/FASDH25/python_exercises\\lesson_8.1\\manto_boys'
+print(os.listdir(os.path.join(dir, "lesson_8.1",)))
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    print(os.listdir(os.path.join(dir, "lesson_8.1",)))
+FileNotFoundError: [WinError 3] The system cannot find the path specified: 'Downloads/FASDH25/python_exercises\\lesson_8.1'
+print(os.listdir(os.path.join(dir, "lesson_8.1", )))
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    print(os.listdir(os.path.join(dir, "lesson_8.1", )))
+FileNotFoundError: [WinError 3] The system cannot find the path specified: 'Downloads/FASDH25/python_exercises\\lesson_8.1'
+
+= RESTART: C:\Users\mongo\Downloads\FASDH25\python_exercises\lesson_8.1\masoumeh_seydi_0.py
+The path to the article is: aljazeera_articles/2024-03-28_9276.txt
+
+
+= RESTART: C:\Users\mongo\Downloads\FASDH25\python_exercises\lesson_8.1\masoumeh_seydi_0.py
+The path to the article is: aljazeera_articles/2024-03-28_9276.txt
+Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+
+-----
+
+This live page i
+
+= RESTART: C:\Users\mongo\Downloads\FASDH25\python_exercises\lesson_8.1\masoumeh_seydi_0.py
+The path to the article is: aljazeera_articles/2024-03-28_9276.txt
+Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+
+-----
+
+This live page i
+
+= RESTART: C:\Users\mongo\Downloads\FASDH25\python_exercises\lesson_8.1\masoumeh_seydi_0.py
+The path to the article is: aljazeera_articles/2024-03-28_9276.txt
+Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+
+-----
+
+This live page i
+['Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israeli', 'Israeli']
+
+= RESTART: C:\Users\mongo\Downloads\FASDH25\python_exercises\lesson_8.1\masoumeh_seydi_0.py
+The path to the article is: aljazeera_articles/2024-03-28_9276.txt
+Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+
+-----
+
+This live page i
+['Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israeli', 'Israeli']
+54
+
+==================================== RESTART: C:\Users\mongo\Downloads\FASDH25\python_exercises\lesson_8.1\muhammad_faheem_0.py ===================================
+The path to the article is: aljazeera_articles/2024-03-28_9276.txt
+Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+
+-----
+
+This live page i
+['Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israeli', 'Israeli']
+54
+
+==================================== RESTART: C:\Users\mongo\Downloads\FASDH25\python_exercises\lesson_8.1\muhammad_faheem_0.py ===================================
+The path to the article is: aljazeera_articles/2024-03-28_9276.txt
+Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+
+-----
+
+This live page i
+['Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israeli', 'Israeli']
+54
+There are 54 of Israeli? in the article 2024-03-28_9276.txt
+
+==================================== RESTART: C:\Users\mongo\Downloads\FASDH25\python_exercises\lesson_8.1\muhammad_faheem_0.py ===================================
+The path to the article is: aljazeera_articles/2024-03-28_9276.txt
+Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+
+-----
+
+This live page i
+['Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israeli', 'Israeli']
+54
+There are 54 of Israeli? in the article 2024-03-28_9276.txt
+title:  Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+body:  
+
+==================================== RESTART: C:\Users\mongo\Downloads\FASDH25\python_exercises\lesson_8.1\muhammad_faheem_0.py ===================================
+The path to the article is: aljazeera_articles/2024-03-28_9276.txt
+Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+
+-----
+
+This live page i
+['Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israeli', 'Israeli']
+54
+There are 54 of Israeli? in the article 2024-03-28_9276.txt
+title:  Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+body:  
+Traceback (most recent call last):
+  File "C:\Users\mongo\Downloads\FASDH25\python_exercises\lesson_8.1\muhammad_faheem_0.py", line 63, in <module>
+    print(f"there are {n_matches} of {Pattern} in the article {filename}")
+NameError: name 'Pattern' is not defined. Did you mean: 'pattern'?
+
+==================================== RESTART: C:/Users/mongo/Downloads/FASDH25/python_exercises/lesson_8.1/muhammad_faheem_2.py ===================================
+The path to the article is: aljazeera_articles/2024-03-28_9276.txt
+Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+
+-----
+
+This live page i
+['Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israeli', 'Israeli']
+54
+There are 54 of Israeli? in the article 2024-03-28_9276.txt
+title:  Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+body:  
+Traceback (most recent call last):
+  File "C:/Users/mongo/Downloads/FASDH25/python_exercises/lesson_8.1/muhammad_faheem_2.py", line 63, in <module>
+    print(f"there are {n_matches} of {Pattern} in the article body {filename}")
+NameError: name 'Pattern' is not defined. Did you mean: 'pattern'?
+
+==================================== RESTART: C:/Users/mongo/Downloads/FASDH25/python_exercises/lesson_8.1/muhammad_faheem_2.py ===================================
+The path to the article is: aljazeera_articles/2024-03-28_9276.txt
+Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+
+-----
+
+This live page i
+['Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israeli', 'Israeli']
+54
+There are 54 of Israeli? in the article 2024-03-28_9276.txt
+title:  Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+body:  
+Traceback (most recent call last):
+  File "C:/Users/mongo/Downloads/FASDH25/python_exercises/lesson_8.1/muhammad_faheem_2.py", line 63, in <module>
+    print(f"there are {n_matches} of {Pattern} in the article body {filename}")
+NameError: name 'Pattern' is not defined. Did you mean: 'pattern'?
+
+==================================== RESTART: C:/Users/mongo/Downloads/FASDH25/python_exercises/lesson_8.1/muhammad_faheem_2.py ===================================
+The path to the article is: aljazeera_articles/2024-03-28_9276.txt
+Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+
+-----
+
+This live page i
+['Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israeli', 'Israeli']
+54
+There are 54 of Israeli? in the article 2024-03-28_9276.txt
+title:  Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+body:  
+Traceback (most recent call last):
+  File "C:/Users/mongo/Downloads/FASDH25/python_exercises/lesson_8.1/muhammad_faheem_2.py", line 63, in <module>
+    print(f"there are {n_matches} of {Pattern} in the article body {filename}")
+NameError: name 'Pattern' is not defined. Did you mean: 'pattern'?
+>>> 
+==================================== RESTART: C:/Users/mongo/Downloads/FASDH25/python_exercises/lesson_8.1/muhammad_faheem_2.py ===================================
+The path to the article is: aljazeera_articles/2024-03-28_9276.txt
+Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+
+-----
+
+This live page i
+['Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israeli', 'Israel', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israel', 'Israel', 'Israel', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israeli', 'Israel', 'Israeli', 'Israeli']
+54
+There are 54 of Israeli? in the article 2024-03-28_9276.txt
+title:  Israel’s war on Gaza updates: ICJ again orders Israel to ensure aid to Gaza
+body:  
+there are 52 of Israeli? in the article body 2024-03-28_9276.txt
+there are 52 of Israeli? in the article title 2024-03-28_9276.txt
+there are 52 of Israeli? in the article text 2024-03-28_9276.txt
