2b597493e246dabaee44a9c5bc0c50c113b6a138
kamilkami123 <ahmadkamil.3218@gmail.com>
2025-03-25 18:12:57 +0500

    Add my new files

--- python_exercises/lesson_7.1/manger/kamil_ahmad_count_titles.py.py
+++ python_exercises/lesson_7.1/manger/kamil_ahmad_count_titles.py.py
@@ -1,31 +1,32 @@
-filename = "1350MirzaHadiyRuswa.AmrawJanAda.FASDH2025010-urdu1" 
-
-with open(filename, 'r', encoding='utf8') as file:
+with open("1350MirzaHadiyRuswa.AmrawJanAda.FASDH2025010-urdu1", "r", encoding="utf-8") as file:
     lines = file.readlines()
 
-print("First 10 lines of the file:")
+
+print("First 10 lines:")
 print("".join(lines[:10]))
 
-word_counts = []
-matching_lines = []
 
-for heading in lines:
-    if "###" in heading:
-        matching_lines.append(heading.strip())
+heading_lines = [line.strip() for line in lines if "###" in line]
 
-print("First 3 headings:")
-print("\n".join(matching_lines[:3]))
 
-for heading in matching_lines:
-    print("heading:", heading)
-    words = heading.split()
-    count_words = len(words)
-    word_counts.append(count_words)
-    print("count of the words in heading:", count_words)
+print("\nFirst 3 headings:")
+print("\n".join(heading_lines[:3]))
 
-    print("Word lengths:", [len(word) for word in words])
-    
-first_level_headings = [heading for heading in matching_lines if heading.count('|') == 1]
+
+print("\nWord count in each heading:")
+for heading in heading_lines:
+    word_count = len(heading.split())
+    print(f"Count of words in this heading: {word_count}")
+
+
+print("\nWord lengths in each heading:")
+for heading in heading_lines:
+    word_lengths = [len(word) for word in heading.split()]
+    print(f"Heading: {heading}")
+    print(f"Word lengths: {word_lengths}")
+
+first_level_headings = [heading for heading in heading_lines if heading.count("|") == 1]
+
 
 print("\nFirst-level headings:")
 print("\n".join(first_level_headings))
