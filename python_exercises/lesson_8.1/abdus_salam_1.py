6cd86f20b3fe320a0bcf2f72e5eca775858e8fcd
aabdus2946 <abdus.salam2@scholar.aku.edu>
2025-03-26 05:37:17 +0500

    added new changes

--- python_exercises/lesson_8.1/abdus_salam_1.py
+++ python_exercises/lesson_8.1/abdus_salam_1.py
@@ -40,12 +40,24 @@ with open(file_path, mode="r", encoding="utf8") as file:
 # EXERCISE: print the first 100 characters of the text:
 print(text[:100])
 
-pattren = r"Israeli?"
-matches = re.findall(pattren, text)
+pattern = r"Israeli?"
+matches = re.findall(pattern, text)
 print(matches)
 n_matches = len(matches)
 print(n_matches)
-print(f" There are {n_matches} of {pattren} in the article{filename}")
+print(f" There are {n_matches} of {pattern} in the article{filename}")
+
+splitter_pattern = r"\n+-+\n+"
+split_text = re.split(splitter_pattern, text)
+title = split_text[0]
+body = split_text[1]
+
+matches = re.findall(pattern, body)
+n_matches = len(matches)
+print(f"There are {n_matches} of {pattern} in the article {filename}")
+matches = re.findall(pattern, title)
+n_matches = len(matches)
+print(f"There are {n_matches} of {pattern} in the article {filename}")
 
 
 
