0e90e9fef820e43e0e7b9ee8c477780861e73d75
ihtisham-bit <ihtisham.alihassan@scholar.aku.edu>
2025-03-26 16:25:40 +0500

    i made changes

--- python_exercises/lesson_8.1/ihtisham_alihassan_3.py
+++ python_exercises/lesson_8.1/ihtisham_alihassan_3.py
@@ -48,4 +48,20 @@ print(f"{filename} contains {n_matches} {pattern} timein the article")
 # EXERCISE: print the first 100 characters of the text:
 print()
 
+splitter_pattern = r"\n+-+\n+"
+split_text = re.split(splitter_pattern, text)
+title = split_text[0]
+body = split_text [1]
 
+print("title: ", title)
+print("body: ", body)
+
+matches = re.findall(pattern, body)
+n_matches = len(matches)
+print(f"{filename} contains {n_matches} {pattern} times in the body of the article")
+
+patterns = [r"Israeli?", r"Palestine|Palestinian", r"Gazan?"]
+for pattern in patterns:
+    matches = re.findall(pattern, body)
+    n_matches = len(matches)
+    print(f"{filename} contain {n_matches} of {pattern} times in the body of the article")
