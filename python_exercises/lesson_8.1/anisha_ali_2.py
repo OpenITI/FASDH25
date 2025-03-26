a431ddd50acf6530ac8faa5a6a8e5dbe73560745
anisha.ali@scholar.aku.edu <ammara.karim@scholar.aku.edu>
2025-03-26 22:34:16 +0500

    updated anisha_ali_0.py and added new files

--- python_exercises/lesson_8.1/anisha_ali_2.py
+++ python_exercises/lesson_8.1/anisha_ali_2.py
@@ -48,5 +48,26 @@ print(matches)
 n_matches = len(matches)
 print(f"{filename} contains {n_matches} {pattern} time in the article")
 
+splitter_pattern = r"\n+-+\n+"
+split_text = re.split(splitter_pattern, text)
+title = split_text[0]
+body = split_text[1]
+
+print("titles: ", title)
+print("body: ",body)
+
+matches = re.findall(pattern, body)
+n_matches = len(matches)
+print(f"{filename} contains {n_matches} {pattern} times in the body of the article")
+
+patterns = [r"Israeli?", r"Palestine|Palestinian", r"Gazan?"]
+for pattern in patterns:
+    matches = re.findall(pattern, body)
+    n_matches = len(matches)
+    print(f"{filename} contains {n_matches} of {pattern}")
+
+
+
+
 
 
