910ad6e6f9f606fb7bd396281d6aabdd7770b0eb
Shahzad71717 <srgmisgari11104@gmail.com>
2025-03-26 21:51:06 +0500

    work done

--- python_exercises/lesson_8.1/shahzad_rahim_2.py
+++ python_exercises/lesson_8.1/shahzad_rahim_2.py
@@ -45,5 +45,23 @@ print(matches)
 n_matches = len(matches)
 print(f"{filename} contains {n_matches} {pattern} time in the article")
 
+splitter_pattern = r"\n+-+|b+"
+split_text = re.split(splitter_pattern, text)
+title = split_text[0]
+body = split_text[1]
+
+print("title: ", title)
+print("body: ", body)
+
+matches = re.findall(pattern, body)
+n_matches = len (matches)
+print(f"{filename} contains {n_matches} {pattern} times in the article")
+
+patterns = [r"Israeli?", r"Palestine|Palestinian",r"Gazan"]
+for pattern in patterns:
+    matches = re.findall(pattern, body)
+    n_matches = len(matches)
+    print(f"{filename} contains {n_matches} {pattern} times in the body of the article")
+
 
 
