1cb96ae4ce1f8f15c70d2b6adbea29a673ac2aeb
DELL <shah.zaib@scholar.aku.edu>
2025-03-27 16:50:38 +0500

    python exercise

--- python_exercises/lesson_8.2/shah_zaib_4.py
+++ python_exercises/lesson_8.2/shah_zaib_4.py
@@ -25,7 +25,9 @@ import os
 folder = "aljazeera_articles"
 filename = "2024-03-28_9276.txt"
 
-total = 0 
+patterns = [r"Israel\b", r"Palestine|Palestinian", r"Gazan?"]
+total = [0,0,0]
+
 
 for filename in os.listdir(folder):
     
@@ -41,10 +43,19 @@ for filename in os.listdir(folder):
         text = file.read()
 
     # find all the occurences of Israel or Israeli in the text:
-    pattern = r"Israeli?"
-    matches = re.findall(pattern, text)
-    n_matches = len(matches)
-    print(f"{filename} contains {pattern} {n_matches} times in the article")
-    total += n_matches
-print(f" found {total} matches in the corpus!")
+    
+    #for pattern in patterns
+    for pattern_number in range(len(patterns)):
+        pattern = patterns[pattern_number]
+        print(pattern_number,pattern)
+        matches = re.findall(pattern, text)
+        n_matches = len(matches)
+        print(f"{filename} contains {pattern} {n_matches} times in the article")
+        total[pattern_number] += n_matches
+print(f" we found {total} matches in the corpus!")
+for pattern_number in range(len(patterns)):
+        pattern = patterns[pattern_number]
+        n_times = total[pattern_number]
+        print(f"found {pattern} {n_times} times")
+
 
