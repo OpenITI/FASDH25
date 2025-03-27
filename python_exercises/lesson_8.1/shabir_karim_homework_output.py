388ced8a21a090a8c65efdbd2ab961d0a7981f5d
shabirkarim <shabir1023@gmail.com>
2025-03-27 08:06:12 +0500

    added changes

---
+++ python_exercises/lesson_8.1/shabir_karim_homework_output.py
@@ -0,0 +1,22 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/JMS/Downloads/FASDH25/python_exercises/lesson_8.1/Shabir_Karim_homework.py
+Traceback (most recent call last):
+  File "C:/Users/JMS/Downloads/FASDH25/python_exercises/lesson_8.1/Shabir_Karim_homework.py", line 12, in <module>
+    matches = re.findall (pattern, title)
+NameError: name 'title' is not defined. Did you mean: 'file'?
+>>> 
+= RESTART: C:/Users/JMS/Downloads/FASDH25/python_exercises/lesson_8.1/Shabir_Karim_homework.py
+['Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gazan', 'Gaza', 'Gazan']
+>>> 
+= RESTART: C:/Users/JMS/Downloads/FASDH25/python_exercises/lesson_8.1/Shabir_Karim_homework.py
+['Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gazan', 'Gaza', 'Gazan']
+['Gaza', 'Gaza']
+2
+>>> 
+= RESTART: C:/Users/JMS/Downloads/FASDH25/python_exercises/lesson_8.1/Shabir_Karim_homework.py
+['Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gaza', 'Gazan', 'Gaza', 'Gazan']
+['Gaza', 'Gaza']
+2
+This file 2024-01-15_10035.txt contains 2 of Gazan? in the article's title.
