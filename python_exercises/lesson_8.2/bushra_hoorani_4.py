a18b91c5f3e0ae3fce8a23fb5341707442dff459
BushraHoorani <bushra.hoorani@scholar.aku.edu>
2025-03-28 13:49:50 +0500

    add new changes

--- python_exercises/lesson_8.2/bushra_hoorani_4.py
+++ python_exercises/lesson_8.2/bushra_hoorani_4.py
@@ -25,8 +25,15 @@ import os
 folder = "aljazeera_articles"
 filename = "2024-03-28_9276.txt"
 
+
+patterns = [r"Israel\b", r"Palestine|Palestinian", r"Gazan?"]
+
+total = [0,0,0]
+
 for filename in os.listdir(folder):
     # build the file path:
+    #file_path = f"{folder}/{filename]"
+    file_path = os.path.join(folder, filename)
     file_path = f"{folder}/{filename}"
     print(f"The path to the article is: {file_path}")
 
@@ -34,10 +41,25 @@ for filename in os.listdir(folder):
     with open(file_path, encoding="utf-8") as file:
         text = file.read()
 
-    # find all the occurences of Israel or Israeli,
-    # Palestine or Palestinian, and Gaza or Gazan in the text:
-    patterns = (r"Israeli?", r"palestine|Palestinian", r"Gazan?")
-    for pattern in patterns:
+    # load the article (text file) into Python:
+    with open(file_path, encoding="utf-8") as file:
+        text = file.read()
+
+    # find all the occurences of Israel or Israeli in the text:
+    #for pattern in patterns:
+    for pattern_number in range(len(patterns)):
+        pattern = patterns[pattern_number]
+        print(pattern_number, pattern)
         matches = re.findall(pattern, text)
         n_matches = len(matches)
         print(f"{filename} contains {pattern} {n_matches} times in the article")
+        total[pattern_number] += n_matches
+
+print(f"we found {total} matches in the corpus!")
+
+
+
+for pattern_number in range(len(patterns)):
+        pattern = patterns[pattern_number]
+        n_times = total[pattern_number]
+        print(f"we found {pattern} {n_times} times")
