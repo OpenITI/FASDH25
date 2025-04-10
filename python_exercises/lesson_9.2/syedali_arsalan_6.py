172e2fd68aeac88ec7bdfbb45d574624101610d5
syedarslan476 <syedarslan476@gmail.com>
2025-04-10 15:38:00 +0500

    added new file

--- python_exercises/lesson_9.2/syedali_arsalan_6.py
+++ python_exercises/lesson_9.2/syedali_arsalan_6.py
@@ -23,11 +23,25 @@ import os
 folder = "aljazeera_articles"  
 
 # define the patterns we want to search:
-patterns = {
-    r"Israeli?": 0,
-    r"Palestine|Palestinian": 0,
-    r"Gazan?": 0
-}
+gazetteer_path = "gazetteers/geonames_gaza_selection.tsv"
+with open(gazetteer_path, encoding="utf-8") as file:
+    data = file.read()
+
+
+rows = data.split("\n")
+
+patterns = {}
+for row in rows[1:]:
+    columns = row.split("\t")
+    place = columns[0]
+    print(place)
+    patterns[place] = 0
+
+print(patterns)
+
+input()
+
+
 
 for filename in os.listdir(folder):
     # build the file path:
@@ -44,5 +58,6 @@ for filename in os.listdir(folder):
         n_matches = len(matches)
         patterns[pattern] += n_matches
 for pattern, count in patterns.items():
-    print(f"Found {pattern} {count}  times")
+    if count > 0:
+       print(f"Found {pattern} {count}  times")
         
