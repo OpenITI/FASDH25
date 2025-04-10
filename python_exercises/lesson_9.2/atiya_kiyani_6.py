ee1af27288891a95641ef5ff50a06fbea986adaa
atiya955 <atiya.kiyani@scholar.aku.edu>
2025-04-10 15:46:18 +0500

    add places in gaza

--- python_exercises/lesson_9.2/atiya_kiyani_6.py
+++ python_exercises/lesson_9.2/atiya_kiyani_6.py
@@ -23,11 +23,21 @@ import os
 folder = "aljazeera_articles"  
 
 # define the patterns we want to search:
-patterns = {
-    r"Israeli?": 0,
-    r"Palestine|Palestinian": 0,
-    r"Gazan?": 0
-}
+gazetteer_path = "gazetteers/geonames_gaza_selection.tsv"
+with open(gazetteer_path, encoding="utf-8") as file:
+    data = file.read()
+print(data)
+
+rows = data.split("\n")
+
+patterns = {}
+for row in rows[1:]:
+    print(row)
+    columns = row.split("\t")
+    place = columns[0]
+    print(place)
+    patterns[place] = 0
+
 
 for filename in os.listdir(folder):
     # build the file path:
@@ -45,4 +55,5 @@ for filename in os.listdir(folder):
         patterns[pattern] += n_matches
 
 for pattern, count in patterns.items():
-    print(f"Found {pattern} {count} times")
+    if count > 0:
+        print(f"found {pattern} {count} times")
