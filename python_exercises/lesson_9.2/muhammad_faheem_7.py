30681c91a378d20da7206b34b299f68495e33de0
Muhammad Faheem <mohdfaheem.41826@gmail.com>
2025-04-10 15:39:16 +0500

    add my file

--- python_exercises/lesson_9.2/muhammad_faheem_7.py
+++ python_exercises/lesson_9.2/muhammad_faheem_7.py
@@ -15,6 +15,23 @@ The selection criteria of the subset we are working on today are:
 2. The articles contain at least 9 place names in Gaza
 
 '''
+'''This is your starting script for today's Python class.
+
+In this Python class we will explore a collection of articles from
+the website Al Jazeera English about Israel and Palestine.
+
+You will find the articles in the "aljazeera_articles" subfolder.
+
+The collection of articles we are going to look at is only a selection
+of a larger dataset published by Inacio Vieira on Kaggle,
+a repository for data and code for machine learning.
+https://www.kaggle.com/datasets/inaciovieira/al-jazeera-english-israel-gaza-war-from-7th-oct-23
+
+The selection criteria of the subset we are working on today are:
+1. The articles were written from 2023 onwards
+2. The articles contain at least 9 place names in Gaza
+
+'''
 import re
 import os
 
@@ -23,10 +40,26 @@ import os
 folder = "aljazeera_articles"  
 
 # define the patterns we want to search:
-patterns = {r"Israeli?": 0,
-            r"Palestine|Palestinian": 0,
-            r"Gazan?": 0
-            }
+gazetteer_path = "gazetteers/geonames_gaza_selection.tsv"
+with open(gazetteer_path, encoding="utf-8") as file:
+    data = file.read()
+print(data)
+
+rows = data.split("\n")
+
+patterns = {}
+for row in rows[1:]:
+    print(row)
+    colums = row.split("\t")
+    place = colums [0]
+    print(place)
+    patterns[place] = 0
+    
+print(patterns)
+
+input()
+
+
 
 for filename in os.listdir(folder):
     # build the file path:
@@ -36,14 +69,16 @@ for filename in os.listdir(folder):
     # load the article (text file) into Python:
     with open(file_path, encoding="utf-8") as file:
         text = file.read()
+    tagged_text = text 
 
     # find all the occurences of the patterns in the text:
     for pattern in patterns:
         matches = re.findall(pattern, text)
         n_matches = len(matches)
         patterns[pattern] += n_matches
+        tagged_text = re.sub(pattern, "+++ " + pattern, tagged_text)
+    print(tagged_text)
 
 for pattern, count in patterns.items():
-    print(f"Found {pattern} {count} times")
-            
-            
+    if count > 0:
+       print(f"Found {pattern} {count} times")
