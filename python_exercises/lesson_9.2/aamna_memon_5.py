14a9c59b4c3634a9cdbf1aa707bfc56e232a1da4
aamnamemonhyder <aamnamemonhyder@gmail.com>
2025-04-27 01:13:43 +0500

    class exercises

--- python_exercises/lesson_9.2/aamna_memon_5.py
+++ python_exercises/lesson_9.2/aamna_memon_5.py
@@ -1,51 +1,34 @@
-'''This is your starting script for today's Python class.
-
-In this Python class we will explore a collection of articles from
-the website Al Jazeera English about Israel and Palestine.
-
-You will find the articles in the "aljazeera_articles" subfolder.
-
-The collection of articles we are going to look at is only a selection
-of a larger dataset published by Inacio Vieira on Kaggle,
-a repository for data and code for machine learning.
-https://www.kaggle.com/datasets/inaciovieira/al-jazeera-english-israel-gaza-war-from-7th-oct-23
-
-The selection criteria of the subset we are working on today are:
-1. The articles were written from 2023 onwards
-2. The articles contain at least 9 place names in Gaza
-
-'''
 import re
 import os
 
-# define which folder to use:
-# NB: these are different articles than in the previous weeks
 folder = "aljazeera_articles"  
-
-# define the patterns we want to search:
 gazetteer_path="gazetteers/geonames_gaza_selection.tsv"
+
 with open(gazetteer_path, encoding="utf-8") as file:
     data = file.read()
+print(data,'\n')
 
-    
-print(data)
-input("CONTINUE")
+print('------------------------------------------------')
 
+dictionary= {r'Israeli?':0,
+           r'Palestine|Palestinian':0,
+           r'Gazan?':0}
 
 for filename in os.listdir(folder):
-    # build the file path:
     file_path = f"{folder}/{filename}"
+    
     print(f"The path to the article is: {file_path}")
+    
 
-    # load the article (text file) into Python:
     with open(file_path, encoding="utf-8") as file:
         text = file.read()
 
-    # find all the occurences of the patterns in the text:
-    for pattern in patterns:
-        matches = re.findall(pattern, text)
-        n_matches = len(matches)
-        patterns[pattern] += n_matches
+    for pattern_key in dictionary:
+        matches = re.findall(pattern_key, text)
+        no_of_matches = len(matches)
+        print(no_of_matches, pattern_key, '\n')
+        dictionary[pattern_key] += no_of_matches
         
-for pattern, count in patterns.items():
-    print(f"found {patterns} {patterns[pattern]} times")
+for pattern_key, count in dictionary.items():
+    print(f"found {pattern_key} {count} times")
+
