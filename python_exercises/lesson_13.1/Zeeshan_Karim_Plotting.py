ea39f94209e210f099d88e747487347e27140f9a
ZeeshanKarim-916 <zeeshan.karim2@scholar.aku.edu>
2025-05-12 23:31:33 +0500

    Add changes

---
+++ python_exercises/lesson_13.1/Zeeshan_Karim_Plotting.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import plotly.express as px
+
+# Reading the CSV file
+df = pd.read_csv("data/title.csv")
+
+# Creating the histogram
+fig = px.histogram(
+    df,
+    x="length",
+    title="Article lengths in the Gaza Corpus",
+    color = "year",
+    labels={"length": "Length in tokens"}
+)
+
+# Add tick marks to x-axis
+fig.update_xaxes(ticks="inside", tickwidth=2)
+
+# Update the y-axis label to "Frequency"
+fig.update_yaxes(title_text="Frequency")
+
+# Show the figure
+fig.show()
