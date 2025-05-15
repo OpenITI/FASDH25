9b22e4d5e546d9eedc689377a2b1be7fa3fe05d4
Muhammad Faheem <mohdfaheem.41826@gmail.com>
2025-05-15 10:55:42 +0500

    add my files

--- python_exercises/lesson_14.1/muhammad_faheem-ngrams1.py
+++ python_exercises/lesson_14.1/muhammad_faheem-ngrams1.py
@@ -1,18 +1,28 @@
-
-import plotly.express as px
+# Import necessary libraries
 import pandas as pd
+import plotly.express as px
 
-# Load the CSV file in Dataframe
+# Load the CSV file into a pandas DataFrame
 df = pd.read_csv("data/1-gram.csv")
 
-# Print the first 10 rows of the Dataframe
+# Print the first 10 rows to inspect the data
 print(df.head(10))
 
-# Filter the Dataframe to only include relevent n-grams
-filter = df["1-gram"].isin(["peace", "agreement", "truce"])
-print(filter)
-df2 = df[ filter ]
+#sorting the top 10 frequency
+top_10= df.sort_values(by='count', ascending=False).head(10)
+#printing the top 10 frequency row
+print(top_10)
 
+#subset of the dataframe
+df2= df[df ["1-gram"].isin( ["peace" , "agreement" , "truce"])]
+print(df2)
 
-fig = px.line(df2, x="month", y="count")
+fig = px.line(
+    df2,
+    x="month",
+    y="count",
+    title="Monthly Frequency of Selected Unigrams",
+    labels={"month": "Month", "frequency": "Frequency", "1-gram": "Unigram"}
+)
+
 fig.show()
