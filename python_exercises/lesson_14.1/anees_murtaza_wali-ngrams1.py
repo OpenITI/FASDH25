ddd9277de8cb1f635b65b259d62cb3b41a445889
unknown <anees.murtazawali@scholar.aku.edu>
2025-05-15 11:08:45 +0500

    Added new changes

--- python_exercises/lesson_14.1/anees_murtaza_wali-ngrams1.py
+++ python_exercises/lesson_14.1/anees_murtaza_wali-ngrams1.py
@@ -7,11 +7,16 @@ df = pd.read_csv("data/1-gram.csv")
 # Print the first 10 rows of the Dataframe
 print(df.head(10))
 
+# Sorting the top 10 frequencies
+top_10 = df.sort_values (by='count', ascending=False).head(10)
+print(top_10)
+
 # Filter the Dataframe to only include relevent n-grams
 filter = df["1-gram"].isin(["peace", "agreement", "truce"])
 print(filter)
 df2 = df[ filter ]
+print(df2)
 
-
-fig = px.line(df2, x="month", y="count")
+# Plot the line graph
+fig = px.line(df2, x="month", y="count", color="1-gram")
 fig.show()
