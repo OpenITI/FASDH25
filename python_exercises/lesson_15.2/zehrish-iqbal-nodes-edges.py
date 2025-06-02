976bb9c8ace8b59e8b77518ccadc4ed1b4471296
ZehrishIqbal <zehrish.iqbal@scholar.aku.edu>
2025-06-02 14:11:36 +0500

    Added edges and nodes class exercise

--- python_exercises/lesson_15.2/zehrish-iqbal-nodes-edges.py
+++ python_exercises/lesson_15.2/zehrish-iqbal-nodes-edges.py
@@ -9,25 +9,25 @@ df = pd.read_csv("data/tfidf-2024.csv")
 #create the edges list
 edges_df = df.rename(columns={"filename-1": "Source",
                               "filename-2": "Target",
-                              "Similarity": "Weight"})
+                              "similarity": "Weight"})
 
-edges_df = edges_df[["Source", Target", "Weight"]]
+edges_df = edges_df[["Source", "Target", "Weight"]]
 print(edges_df)
 edges_df.to_csv("outputs/Zehrish-Iqbal-edges.csv", encoding="utf-8-sig",
                 index=False)
 
 
-source_nodes = df.renamed(columns={"filename-1": "Id",
-                                   "title-1": "Lable",
+source_nodes = df.rename(columns={"filename-1": "Id",
+                                   "title-1": "Label",
                                    "month-1": "month"})
 source_nodes = source_nodes[["Id", "Label", "month"]]
 
 target_nodes = df.rename(columns={"filename-2": "Id",
                                   "title-2": "Label",
-                                  "month-2": "month"{)
+                                  "month-2": "month"})
 
-target_notes = target_notes[["Id", "Label", "month"]]
-
+target_notes = target_nodes[["Id", "Label", "month"]]
+#combine and drop dublicates
 nodes = pd.concat([source_nodes, target_nodes])
                                   
 nodes = nodes.drop_duplicates()
